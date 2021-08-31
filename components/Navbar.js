191b7e6bd96851612220b2b09ae7994ad51f39ba
Triston <tristonherbst97@gmail.com>
2021-08-31 16:02:47 -0500

    styling for the layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,25 @@
 import Link from "next/link";
-import logo from "../public/logo.png"
+import Image from 'next/image'
+import logo from "../public/logo.png";
 import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
-    
   return (
-    <nav className='nav-bar'>
+    <nav className="nav-bar">
       <div className="logo">
-        <img source={logo}/>
+        <Image src={logo} alt="logo" />
       </div>
       <Link href="/">
         <a>Home</a>
       </Link>
       <Link href="/about">
-        <a>Lessons and Classes</a>
+        <a>Juniors</a>
+      </Link>
+      <Link href="/about">
+        <a>Adults</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
       </Link>
       <Link href="/staff">
         <a>Staff</a>
